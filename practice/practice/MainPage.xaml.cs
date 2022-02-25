b007e472ad8987cc0f827fca5a908b5b86ddf5e5
hafeez <hafeezahamed5074@gmail.com>
2022-02-25 18:01:49 +0530

    xamarin

---
+++ practice/practice/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace practice
+{
+    public partial class MainPage : ContentPage
+    { 
+        public MainPage()
+        { 
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_Calculate(object sender, EventArgs e)
+        {
+            double total = Convert.ToDouble(principle.Text) + Convert.ToDouble(principle.Text) * Convert.ToDouble(Months.Text) * Convert.ToDouble(intrest.Text) / 100;
+            lbltotal.Text = total.ToString();
+        }
+
+        
+    }
+}
